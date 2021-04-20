3d15a32c3554d12795195ea6c3d010a868b8810f
Mathias Fuchs <mathias_fuchs_87@yahoo.de>
2021-04-20 14:23:30 +0200

    advanced prettified

--- advanced.php
+++ advanced.php
@@ -19,19 +19,49 @@
             switch ($celsius) {
 
                 case ($celsius < 5):
-                    echo "$celsius celsius is very cold";
+                    echo '<div class="card" style="width: 18rem;">
+                    <img src="https://alltop.com/viral/wp-content/uploads/2014/01/Fotolia_2429571_Subscription_L-760x430.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">'.$celsius.'° Celsius</h5>
+                      <p class="card-text">Is very Cold</p>          
+                    </div>
+                  </div>';
                     break;
-                case ($celsius > 5 && $celsius < 10):
-                    echo "$celsius celsius is cold";
+                case ($celsius > 6 && $celsius < 10):
+                    echo '<div class="card" style="width: 18rem;">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgAapJm2V5u5WYwI4H6jFV_KzqzITdWJ0fdg&usqp=CAU" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">'.$celsius.'° Celsius</h5>
+                      <p class="card-text">Is Cold</p>          
+                    </div>
+                  </div>';
                     break;
-                case ($celsius > 10 && $celsius < 15):
-                    echo "$celsius celsius is pleasant";
+                case ($celsius > 11 && $celsius < 15):
+                    echo '<div class="card" style="width: 18rem;">
+                    <img src="https://media-cdn.tripadvisor.com/media/photo-s/09/22/b7/ee/inn-at-pleasant-lake.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">'.$celsius.'° Celsius</h5>
+                      <p class="card-text">Is Pleasant</p>          
+                    </div>
+                  </div>';
                     break;
-                case ($celsius > 15 && $celsius < 20):
-                    echo "$celsius celsius is warm";
+                case ($celsius > 16 && $celsius < 21):
+                    echo '<div class="card" style="width: 18rem;">
+                    <img src="https://www.jetsetter.com/wp-content/uploads/sites/7/2018/04/zjCUOJIL.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">'.$celsius.'° Celsius</h5>
+                      <p class="card-text">Is Warm</p>          
+                    </div>
+                  </div>';
                     break;
-                case ($celsius > 20):
-                    echo "$celsius celsius is hot";
+                case ($celsius > 22):
+                    echo '<div class="card" style="width: 18rem;">
+                    <img src="https://specials-images.forbesimg.com/imageserve/824845572/960x0.jpg?fit=scale" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">'.$celsius.'° Celsius</h5>
+                      <p class="card-text">Is Hot</p>          
+                    </div>
+                  </div>';
                     break;
                 default:
                     echo "meh";
